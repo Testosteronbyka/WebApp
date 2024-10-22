392f431cb5e8a8b59965c3c66edc451a4acc46bb
testosteronbyka <dominikkorbiel5@gmail.com>
2024-10-22 17:47:43 +0200

    3

---
+++ WebApp/Controllers/ContactController.cs
@@ -0,0 +1,67 @@
+using Microsoft.AspNetCore.Mvc;
+using WebApp.Models;
+
+namespace WebApp.Controllers;
+
+public class ContactController : Controller
+{
+
+    private static Dictionary<int, ContactModel> _contacts = new()
+    {
+        {
+            1,
+            new ContactModel()
+            {
+                Id = 1,
+                FirstName = "Adamo",
+                LastName = "Kus",
+                Email = "adamkus@wsei.edu.pl",
+                PhoneNumber = "333 134 003",
+                BirthDate = new DateOnly(2003, 10, 10)
+            }
+        },
+        {
+            2,
+            new ContactModel()
+            {
+                Id = 2,
+                FirstName = "Michal",
+                LastName = "Glus",
+                Email = "michalglus@wsei.edu.pl",
+                PhoneNumber = "885 267 388",
+                BirthDate = new DateOnly(2005, 3, 22)
+            }
+        },
+        {
+            3,
+            new ContactModel()
+            {
+                Id = 3,
+                FirstName = "Kamil",
+                LastName = "Zdun",
+                Email = "kamilzdun@wsei.edu.pl",
+                PhoneNumber = "578 399 100",
+                BirthDate = new DateOnly(2000, 1, 5)
+            }
+        }
+    };
+    // GET
+    public IActionResult Index()
+    {
+        return View(_contacts);
+    }
+    
+    public IActionResult Add()
+    {
+        return View();
+    }
+    
+    public IActionResult Add(ContactModel model)
+    {
+        if (!ModelState.IsValid)
+        {
+            return View();
+        }
+        return View();
+    }
+}
