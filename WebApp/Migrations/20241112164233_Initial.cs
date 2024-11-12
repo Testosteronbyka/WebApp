e7312a25812e966dee509f90f1c9ba56853aed08
testosteronbyka <dominikkorbiel5@gmail.com>
2024-11-12 18:22:54 +0100

    2

---
+++ WebApp/Migrations/20241112164233_Initial.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApp.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "organizations",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false),
+                    NIP = table.Column<string>(type: "TEXT", nullable: false),
+                    REGON = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_City = table.Column<string>(type: "TEXT", nullable: false),
+                    Address_Street = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_organizations", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "contacts",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    FirstName = table.Column<string>(type: "TEXT", maxLength: 20, nullable: false),
+                    LastName = table.Column<string>(type: "TEXT", maxLength: 50, nullable: false),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    PhoneNumber = table.Column<string>(type: "TEXT", nullable: false),
+                    birth = table.Column<DateOnly>(type: "TEXT", nullable: false),
+                    Created = table.Column<DateTime>(type: "TEXT", nullable: false),
+                    OrganizationId = table.Column<int>(type: "INTEGER", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_contacts", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_contacts_organizations_OrganizationId",
+                        column: x => x.OrganizationId,
+                        principalTable: "organizations",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.InsertData(
+                table: "organizations",
+                columns: new[] { "Id", "Address_City", "Address_Street", "NIP", "Name", "REGON" },
+                values: new object[,]
+                {
+                    { 101, "Kraków", "Św. filipa 17", "098482899", "WSEI", "09786179841" },
+                    { 102, "Warszawa", "Dworcowa 8", "893894618", "PKP", "6894386829" }
+                });
+
+            migrationBuilder.InsertData(
+                table: "contacts",
+                columns: new[] { "Id", "birth", "Created", "Email", "FirstName", "LastName", "OrganizationId", "PhoneNumber" },
+                values: new object[,]
+                {
+                    { 1, new DateOnly(2000, 10, 10), new DateTime(2024, 11, 12, 17, 42, 32, 906, DateTimeKind.Local).AddTicks(8120), "ewabak@gmail.com", "ewa", "bak", 101, "497290407" },
+                    { 2, new DateOnly(1997, 10, 10), new DateTime(2024, 11, 12, 17, 42, 32, 906, DateTimeKind.Local).AddTicks(8166), "vbsruihbuiwhak@gmail.com", "flf", "bhfyuk", 102, "497085668" }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_contacts_OrganizationId",
+                table: "contacts",
+                column: "OrganizationId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "contacts");
+
+            migrationBuilder.DropTable(
+                name: "organizations");
+        }
+    }
+}
